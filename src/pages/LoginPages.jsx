8f5254f5c65e1c6b44a59a6ec546291e0c305c9b
axelsameul <emi.duarte643@gmail.com>
2023-08-29 14:21:10 -0300

    nuevos cambios en login y css

--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -5,17 +5,20 @@ const LoginPages = () => {
 const [formInput,setFormsInput]= useState({
 
     user:"",
-    pass:""
+    pass:"",
+    rPass:""
 })
 const [userInput,setUserInput]=useState(false)
 const[passInput,setPassInput]=useState(false)
-
+const [rpassInput ,setRpassInput] = useState(false)
 const handleChangue = (ev)=>{
 setFormsInput({...formInput,[ev.target.name]: ev.target.value})
 if (formInput.user) {
     setUserInput(false)
   }else if(formInput.pass){
     setUserInput(false)
+  }else if (formInput.rPass) {
+    setRpassInput(false)
   }
 }  
 
@@ -23,8 +26,11 @@ const handleClik = (ev)=>{
 ev.preventDefault()
 if (formInput.user) {
     if (formInput.pass) {
-       setUserInput(false)
-       setPassInput(false)
+      if (formInput.rPass) {
+        setUserInput(false)
+        setPassInput(false)
+        setRpassInput(false)
+      }
        
     }
     
@@ -33,6 +39,7 @@ if (formInput.user) {
 }else{
     setUserInput(true)
     setPassInput(true)
+    setRpassInput(true)
   }
 
 }
@@ -46,7 +53,11 @@ useEffect(()=>{
 
 
   return (
+  
     <>
+   
+   
+    
     <div className='d-flex justify-content-center'>
 <form>
   <div className="mb-3">
@@ -58,6 +69,10 @@ useEffect(()=>{
     <label for="exampleInputPassword1" className="form-label">contraseña</label>
     <input type="password" className={passInput ? "form-control is-invalid": "form-control"} name="pass" onChange={handleChangue} id="exampleInputPassword1"/>
   </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label"> repetir contraseña</label>
+    <input type="password"  className={rpassInput ? "form-control is-invalid": "form-control"} name="pass" onChange={handleChangue} id="exampleInputPassword1"/>
+  </div>
   <button type="submit" className="btn btn-primary"  onClick={handleClik}>iniciar secion</button>
   <a className='ms-3'>recuperar contraseña</a>
 
@@ -66,13 +81,19 @@ useEffect(()=>{
 </div>
 <div className='container  d-flex justify-content-center'  >
 <div className=' mt-5'> 
-<img src="/public/Gmail_29991.ico" alt=""  className='gmil-a'/>
+
+<button className='paf-gmail'>continuar con gmail<img src="/public/Gmail_29991.ico" alt=""  className='gmil-a ms-1' /></button>
 </div>
 <div className=' mt-5 ms-5'> 
-<img src="/public/facebook.png" alt=""  className='gmil-a'/>
+
+<button className='para-face'>continuar con facebook  <img src="/public/facebook.png"  alt="" className='gmil-a ms-1'/></button>
 </div>
 </div>
+
+
     </>
+   
+
   )
 }
 
