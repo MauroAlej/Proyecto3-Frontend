2eb2e53c10056789b38a81a291fd09dde4279868
carla <carlialcorta.ca@gmail.com>
2023-09-11 19:26:25 -0300

    agregor formulario de reserva

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import {BrowserRouter as Router} from "react-router-dom"
 import RoutesView from "./routes/RoutesView";
+
+
+
+
 const App =() => {
 
   return(
     <Router>
     <Navbar/>
  <RoutesView/>
+
  
   </Router>
   )
--- src/pages/Reserva.jsx
+++ src/pages/Reserva.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import FormReserva from '../components/FormReserva'
+
 
 const Reserva = () => {
+
+
   return (
-    <div>Reserva</div>
+  
+    <FormReserva/>
   )
 }
 
