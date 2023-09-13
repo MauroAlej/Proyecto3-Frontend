c25cfb5a32ed37de5ecba69547fd1920d277769c
carla <carlialcorta.ca@gmail.com>
2023-09-13 20:40:58 -0300

    modifico rutas y agrego admin page y componentes de tablas

--- src/routes/RoutesView.jsx
+++ src/routes/RoutesView.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import LoginPages from '../pages/LoginPages'
 import RegisterPage from '../pages/RegisterPage'
 import HomePage from '../page/HomePage'
+import Reserva from '../pages/Reserva'
 
 
 const RoutesView = () => {
@@ -11,6 +12,7 @@ const RoutesView = () => {
       <Route path='/' element={<HomePage/>}/>
     <Route path='/login' element={<LoginPages/>}/>
     <Route path='/register' element={<RegisterPage/>}/>
+    <Route path='/reserva' element={<Reserva/>}/>
 
     </Routes>
   )
