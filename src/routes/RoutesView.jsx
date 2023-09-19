8228798eacf27fd1adb146076ca30839da5f1fce
maurojuarez27851@gmail.com <maurojuarez27851@gmail.com>
2023-09-18 21:09:23 -0300

    se arreglan errores de instalacion

--- src/routes/RoutesView.jsx
+++ src/routes/RoutesView.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import React from 'react';
 // import {Routes, Route} from "react-router-dom";
 import LoginPages from '../pages/LoginPages';
