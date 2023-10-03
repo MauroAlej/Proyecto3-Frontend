a60709ac28ede1ca9c915426bcb6709901d88851
axelsameul <emi.duarte643@gmail.com>
2023-10-03 02:12:00 -0300

    cambios en footer y register y login

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+   <div className='justify-content-center d-flex class-footer'> 
+   <div >
+        <p className='text-dark mt-3 me-3'>El mejor restaurante de <br />comida  china de argentina</p>
+    </div>
+    <div>
+        <img src="../public/kimi.png" alt=""  width={"100px"}/>
+    </div>
+   </div>
+    </>
+  )
+}
+
+export default Footer
